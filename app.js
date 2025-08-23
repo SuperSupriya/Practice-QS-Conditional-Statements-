8a56859a4abb12b5daecd944b3f31972319197fc
SuperSupriya <supriyasingh0941@gmail.com>
2025-08-24 00:03:16 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let size = "XXL";
+if(size === "XL") {
+    console.log("price is Rs 250");
+} 
+else if(size === "L") {
+    console.log("price is Rs 200");
+}
+else if(size === "M") {
+    console.log("price is Rs 50");
+}
+else {
+    console.log("size not available");
+}
